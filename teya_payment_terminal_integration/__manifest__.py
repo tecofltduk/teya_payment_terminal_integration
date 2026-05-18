ae73cce1b41542ea70adbd833007d8e9245f9f40
tecofltduk <tecofltduk@gmail.com>
2026-05-18 17:25:13 +0100

    Add files via upload

---
+++ teya_payment_terminal_integration/__manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# Module developed for Teya integration requirements
+# This module extends Odoo's payment framework
+# Odoo is a trademark of Odoo S.A.
+
+{
+    "name": " Teya Payment Terminal Odoo Point of sale Integration ",
+    "version": "17.0.0.1",
+    'category': 'Sales/Point of Sale',
+    "depends": ['point_of_sale'],
+    "license": "LGPL-3",
+    'website': 'https://tecof.odoo.com',
+    'maintainer': 'Tecof LTD',
+    'summary': "Teya Payment Terminal Odoo Point of sale Integration",
+    "description": " Teya Payment Terminal Odoo Point of sale Integration."
+                   "This module adds options to receive payment throught Teya"
+                   "Terminal and sync the transaction details to POS session "
+                   "automatically. ",
+    "data": [
+        'security/ir.model.access.csv',
+        'data/ir_cron.xml',
+        'views/pos_config.xml',
+        'views/pos_order.xml',
+        'views/pos_payment_methos.xml',
+        'views/res_pos_configuration.xml',
+        'views/teya_pos_store.xml',
+        'views/teya_store_terminals.xml',
+        'wizard/auth_wizard.xml',
+    ],
+    'assets': {
+        'point_of_sale._assets_pos': [
+            'teya_payment_terminal_integration/static/src/**/*',
+        ],
+    },
+    'images': [
+        'static/description/banner.gif',
+    ],
+    'qweb': ['static/src/xml/pos.xml'],
+    "auto_install": False,
+    "installable": True,
+    'application' : True,
+}
---
+++ teya_payment_terminal_integration/static/src/overrides/payment_screen/ticket_screen.js
@@ -0,0 +1,17 @@
+/** @odoo-module **/
+
+import { _t } from "@web/core/l10n/translation";
+import { TicketScreen } from "@point_of_sale/app/screens/ticket_screen/ticket_screen";
+import { patch } from "@web/core/utils/patch";
+
+patch(TicketScreen.prototype, {
+
+    async addAdditionalRefundInfo(order, destinationOrder) {
+        destinationOrder.teya_gateway_payment_id = order.teya_gateway_payment_id;
+        destinationOrder.teya_transaction_id = order.teya_transaction_id;
+        destinationOrder.teya_response = order.teya_response;
+
+        await super.addAdditionalRefundInfo(...arguments);
+    }
+
+})
