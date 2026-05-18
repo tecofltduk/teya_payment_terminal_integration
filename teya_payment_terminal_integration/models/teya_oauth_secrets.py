6cebf447bcc6c1cd538bf9aba602667b377ad0ac
tecofltduk <tecofltduk@gmail.com>
2026-05-18 17:33:15 +0100

    Add files via upload

---
+++ teya_payment_terminal_integration/models/teya_oauth_secrets.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Partner integration reference strings (use tools/wrap_oauth_secrets.py to regenerate).
+# Must use exactly 10-char head + 10-char tail noise (see teya_oauth_padding.py).
+
+PARTNER_REF_ALPHA = 'Q7wE2rT9yUbed6c087-6fb1-4747-9a92-9643c4b9c3cbpL4mN8kJ2x'
+PARTNER_REF_BETA = 'vF3hD6sA1zj16t5svGCB2C-ZqDNJ4yPp7jDsbEdaNaTMAgcRWon04cR0bX5nM7q'
