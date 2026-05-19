e30d64997dc6ad62acdb261d43839576db576ed0
tecofltduk <tecofltduk@gmail.com>
2026-05-19 10:42:43 +0100

    Delete teya_payment_terminal_integration directory

--- teya_payment_terminal_integration/models/teya_oauth_padding.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# Leading / trailing noise length stripped before OAuth calls (vendor configuration).
-
-HEAD_TRIM = 10
-TAIL_TRIM = 10
--- teya_payment_terminal_integration/wizard/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2021-Today: Part of NextFlowIT. 
-# @author:  Part of NextFlowIT. 
-
-from . import auth_wizard
